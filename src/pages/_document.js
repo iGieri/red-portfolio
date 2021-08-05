7856b0fcc9bb7dcb57200d55d49a52dc6c08ff3c
Federico Gerardi <38018471+PyGera@users.noreply.github.com>
2021-08-05 12:38:43 +0200

    Google Ads

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,7 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400&display=swap" rel="stylesheet" />
             <script src="https://kit.fontawesome.com/d256209648.js" crossorigin="anonymous"></script>
+            <script data-ad-client="ca-pub-2464422194774314" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
